f77863f9869d4466e5ebb82d29c901e328b79b68
NehaPatil0712999 <neha2023patil@gmail.com>
2026-04-27 08:59:48 +0530

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -3,21 +3,18 @@
 
 module tb ();
 
-  // Dump waveform
   initial begin
     $dumpfile("tb.fst");
     $dumpvars(0, tb);
     #1;
   end
 
-  // Inputs
   reg clk;
   reg rst_n;
   reg ena;
   reg [7:0] ui_in;
   reg [7:0] uio_in;
 
-  // Outputs
   wire [7:0] uo_out;
   wire [7:0] uio_out;
   wire [7:0] uio_oe;
@@ -27,7 +24,7 @@ module tb ();
   wire VGND = 1'b0;
 `endif
 
-  // Instantiate your Full Adder module
+  // Instantiate your module
   tt_um_full_adder user_project (
 
 `ifdef GL_TEST
@@ -44,30 +41,5 @@ module tb ();
       .clk    (clk),
       .rst_n  (rst_n)
   );
-
-  // Test stimulus
-  initial begin
-    // Initialize
-    clk   = 0;
-    rst_n = 0;
-    ena   = 1;
-    ui_in = 0;
-    uio_in = 0;
-
-    #10 rst_n = 1;
-
-    // Apply all Full Adder combinations (A=ui_in[0], B=ui_in[1], Cin=ui_in[2])
-
-    ui_in = 8'b00000000; #10; // A=0 B=0 Cin=0
-    ui_in = 8'b00000001; #10; // A=1 B=0 Cin=0
-    ui_in = 8'b00000010; #10; // A=0 B=1 Cin=0
-    ui_in = 8'b00000011; #10; // A=1 B=1 Cin=0
-    ui_in = 8'b00000100; #10; // A=0 B=0 Cin=1
-    ui_in = 8'b00000101; #10; // A=1 B=0 Cin=1
-    ui_in = 8'b00000110; #10; // A=0 B=1 Cin=1
-    ui_in = 8'b00000111; #10; // A=1 B=1 Cin=1
-
-    #20 $finish;
-  end
 
 endmodule
